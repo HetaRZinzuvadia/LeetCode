2a38c61da21d2db7b3990326a7d1e2abb38131f7
Heta Rakesh Zinzuvadia <heta3june@gmail.com>
2018-06-03 21:13:05 -0400

    Implement intersect()
    Given two arrays, write a function to compute their intersection.
    Example:
    Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].

---
+++ intersect.js
@@ -0,0 +1,28 @@
+var intersect = function(nums1, nums2) {
+  var hash1 = {};
+  var hash2 = {};
+
+  nums1.forEach((item)=> {
+    if (!hash1[item])
+      hash1[item] = 1; 
+    else 
+      hash1[item]++;
+  });
+
+  nums2.forEach((item)=> {
+    if (!hash2[item])
+      hash2[item] = 1; 
+    else 
+      hash2[item]++;
+  });
+
+  var ans = [];
+
+  for (var k in hash1) {
+    var times = Math.min(hash1[k], hash2[k] || 0);
+    while (times--)
+      ans.push(+k);
+  }
+    
+  return ans;
+};
